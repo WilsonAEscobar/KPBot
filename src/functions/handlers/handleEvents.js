f77423eb9a2ce492d8aea805ea57834a0904ad78
WilsonAEscobar <wilsonescobar909@outlook.com>
2024-06-07 22:47:12 -0500

    adding command and event handlers

---
+++ src/functions/handlers/handleEvents.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+module.exports = (client) => {
+    client .handleEvents = async () =>{
+        const eventFolders = fs.readdirSync('./src/events');
+        for(const folder of eventFolders){
+            const eventFiles = fs.readdirSync(`./src/event/${folder}`).filter(file => file.endsWith('.js'));
+            switch (folder) {
+                case client:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
+};
